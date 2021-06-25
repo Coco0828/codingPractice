8b275c5a96be365a72b52e1fd68005a6f41a8c76
coco0828 <cocowhitetw@gmail.com>
2021-06-25 10:53:54 +0800

    addAnotherFruit

--- snake/snake.js
+++ snake/snake.js
@@ -8,7 +8,16 @@ apple.src = "img/food.png";
 background.src = "img/ground.png";
 let deadSound = new Audio();
 deadSound.src = "./audio/dead.mp3";
-deadSound.play();
+let upSound = new Audio();
+upSound.src = "./audio/up.mp3";
+let downSound = new Audio();
+downSound.src = "./audio/down.mp3";
+let leftSound = new Audio();
+leftSound.src = "./audio/left.mp3";
+let rightSound = new Audio();
+rightSound.src = "./audio/right.mp3";
+let eatSound = new Audio();
+eatSound.src = "./audio/eat.mp3";
 var snake = [];
 snake[0] = { x: 9 * num, y: 10 * num };
 var food = {
@@ -49,6 +58,7 @@ function draw() {
 
     if(snakeX == food.x && snakeY == food.y){
         score ++;
+        eatSound.play();
         food = {
             x: Math.floor(Math.random() * 17 + 1) * num,
             y: Math.floor(Math.random() * 15 + 3) * num
@@ -57,7 +67,8 @@ function draw() {
         snake.pop();
     }
 
-    if(snakeX < num || snakeY > 17 * num || snakeY < 3 * num || snakeY > 17 * num){
+    if(snakeX < num || snakeX > 17 * num || snakeY < 3 * num || snakeY > 17 * num){
+        deadSound.play();
         clearInterval(game);
     }
 
@@ -75,30 +86,34 @@ document.addEventListener("keydown", CTRL);
 function CTRL(event) {
     if (event.keyCode == 37 && d != "right") {       //left
         d = "left";
+        leftSound.play();
     }
     if (event.keyCode == 38 && d != "down") {       //up
         d = "up";
+        upSound.play();
     }
     if (event.keyCode == 39 && d != "left") {       //right
         d = "right";
+        rightSound.play();
     }
     if (event.keyCode == 40 && d != "up") {       //down
         d = "down";
+        downSound.play();
     }
 }
 
 function collision(newHead, snake){
     for(var j = 0; j < snake.length; j++){
         if(newHead.x == snake[j].x && newHead.y == snake[j].y){
-            return true;
+            deadSound.play();
+            clearInterval(game);
         }
     }
-    return false;
 }
 
 ctx.fillStyle = "#921AFF";
 ctx.fillRect(5 * num, 5 * num, num, num);
 
-var game = setInterval(draw, 100);
+var game = setInterval(draw, 150);
 
 //console.log(apple);
