1a226e60344245fd735e43315518d7dfa98fcbfa
coco0828 <cocowhitetw@gmail.com>
2022-12-19 19:27:33 +0800

    20221212C++

--- C++/20221212/e780.cpp
+++ C++/20221212/e780.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-	int n, x = 0, ans = 0, k = 0;
+	int n, x = 0, ans = 0;
 	cin >> n;
 	
 	while(n){
@@ -14,12 +14,8 @@ int main(){
 		if(input == "L") x --;
 		else if(input == "R") x ++;
 		else{
-			if(k){
-				k = 0;
-				int p;
-				stringstream(input) >> p;
-				if(x == p) ans ++; 	
-			} else k = 1;
+			int p;
+			cin >> p, ans += (p == x);
 		}
 		
 		n --;
