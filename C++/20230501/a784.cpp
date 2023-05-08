47d939b3faf9b5285bc02e30cc16367481c0daf3
coco0828 <cocowhitetw@gmail.com>
2023-05-08 19:31:31 +0800

    20230501C++

---
+++ C++/20230501/a784.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string input;
+	while(getline(cin, input) && input != "END"){
+		int k = 0;
+		char *ptr;
+		char a, b;
+		ptr = strtok(input, "-");
+		while(ptr != NULL){
+			if(k == 0){
+				a = ptr
+			}
+		}
+		
+	}
+	
+	return 0;
+}
